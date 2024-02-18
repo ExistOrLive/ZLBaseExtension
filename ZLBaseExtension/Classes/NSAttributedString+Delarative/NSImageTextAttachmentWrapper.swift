bb25c16c2e48e69fdef502568ba1b7e716a8cf15
ExistOrLive <2068531506@qq.com>
2024-02-18 19:22:26 +0800

    【修改信息】富文本图片支持YYText

--- ZLBaseExtension/Classes/NSAttributedString+Delarative/NSImageTextAttachmentWrapper.swift
+++ ZLBaseExtension/Classes/NSAttributedString+Delarative/NSImageTextAttachmentWrapper.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Foundation
+import YYText
 
 public enum NSTextAttachmentWrapperAlignment: Int {
     case baseline
@@ -23,7 +24,7 @@ public class NSImageTextAttachmentWrapper: NSObject {
     // 默认font
     var font: UIFont = UIFont.systemFont(ofSize: 14)
 
-    //
+    // 图片大小
     var size: CGSize = CGSize.zero
 
     // 对齐方式
@@ -31,32 +32,44 @@ public class NSImageTextAttachmentWrapper: NSObject {
 
     // 图片大小适应行高 true： size 将失效
     var fitLineHeight: Bool = false
+    
+    // 是否用于YYText
+    var useInYYText: Bool = false
 
     public init(image: UIImage) {
         self.image = image
         super.init()
     }
 
+    @discardableResult
     public func font(_ font: UIFont) -> NSImageTextAttachmentWrapper {
         self.font = font
         return self
     }
 
+    @discardableResult
     public func size(_ size: CGSize) -> NSImageTextAttachmentWrapper {
         self.size = size
         return self
     }
 
+    @discardableResult
     public func alignment(_ alignment: NSTextAttachmentWrapperAlignment) -> NSImageTextAttachmentWrapper {
         self.alignment = alignment
         return self
     }
 
+    @discardableResult
     public func fitLineHeight(_ fitLineHeight: Bool) -> NSImageTextAttachmentWrapper {
         self.fitLineHeight = fitLineHeight
         return self
     }
 
+    @discardableResult
+    public func useInYYText(_ useInYYText: Bool) -> NSImageTextAttachmentWrapper {
+        self.useInYYText = useInYYText
+        return self
+    }
 }
 
 public extension NSImageTextAttachmentWrapper {
@@ -70,6 +83,51 @@ public extension NSImageTextAttachmentWrapper {
         let attachment = NSTextAttachment()
         attachment.image = image
 
+        /// lineHeight = ascender - descender
+        let lineHeight = font.lineHeight
+        let ascender = font.ascender     /// 在baseline坐标系中，字符顶部的y值
+        let descender = font.descender    /// 在baseline坐标系中，字符底部的y值
+
+        var size = self.size
+        if size == CGSize.zero {
+            size = image.size
+        }
+        if fitLineHeight {
+            if alignment == .baseline {
+                size = CGSize(width: image.size.width * ascender / image.size.height, height: image.size.width * ascender / image.size.height)
+            } else {
+                size = CGSize(width: image.size.width * lineHeight / image.size.height, height: image.size.width * lineHeight / image.size.height)
+            }
+        }
+
+        /// NSTextAttachment 的坐标系是以baseline作为x轴（自左向右），y轴垂直于baseline（自下而上），原点位于文本基线上的字形位置。
+        var bounds = CGRect.zero
+        switch alignment {
+        case .baseline:
+            bounds = CGRect(origin: CGPoint.zero, size: size)
+        case .bottom:
+            bounds = CGRect(origin: CGPoint(x: 0, y: descender), size: size)
+        case .top:
+            bounds = CGRect(origin: CGPoint(x: 0, y: ascender - size.height), size: size)
+        case .centerline:
+            bounds = CGRect(origin: CGPoint(x: 0, y: (ascender + descender - size.height) / 2 ), size: size)
+        }
+        attachment.bounds = bounds
+        return attachment
+    }
+    
+    
+    /**
+       YYText 中显示图片需要使用 NSTextAttachment
+     */
+    @objc dynamic func asYYMutableAttributedString() -> NSMutableAttributedString {
+        let textAttachment = YYTextAttachment()
+        textAttachment.content = image
+        textAttachment.contentMode = .scaleToFill
+        
+        let attributeString = NSMutableAttributedString(string: "\u{FFFC}")
+        attributeString.yy_setTextAttachment(textAttachment, range: NSRange(location: 0, length: attributeString.length))
+        
         let lineHeight = font.lineHeight
         let ascender = font.ascender
         let descender = font.descender
@@ -85,20 +143,31 @@ public extension NSImageTextAttachmentWrapper {
                 size = CGSize(width: image.size.width * lineHeight / image.size.height, height: image.size.width * lineHeight / image.size.height)
             }
         }
-
-        var bounds = CGRect.zero
+        
+        
+        /// YYTextRunDelegate 中 ascent ，descent 均为正值， lineHeight = ascent + descent
+        let delegate = YYTextRunDelegate()
+        delegate.width = size.width
         switch alignment {
         case .baseline:
-            bounds = CGRect(origin: CGPoint.zero, size: size)
+            delegate.ascent = size.height
         case .bottom:
-            bounds = CGRect(origin: CGPoint(x: 0, y: descender), size: size)
-        case .top:
-            bounds = CGRect(origin: CGPoint(x: 0, y: ascender - size.height), size: size)
+            delegate.ascent = size.height + descender
         case .centerline:
-            bounds = CGRect(origin: CGPoint(x: 0, y: (ascender + descender - size.height) / 2 ), size: size)
+            delegate.ascent = size.height / 2 + (ascender - lineHeight / 2)
+        case .top:
+            delegate.ascent = ascender
         }
-        attachment.bounds = bounds
-        return attachment
+        delegate.descent = size.height - delegate.ascent
+        
+        if (delegate.ascent < 0) {
+            delegate.ascent = 0;
+            delegate.descent = size.height;
+        }
+        
+        let delegateRef = delegate.ctRunDelegate()
+        attributeString.yy_setRunDelegate(delegateRef, range: NSMakeRange(0, attributeString.length))
+        return attributeString
     }
 }
 
@@ -108,9 +177,14 @@ extension NSImageTextAttachmentWrapper: NSAttributedStringConvertible {
         self.asMutableAttributedString()
     }
     public func asMutableAttributedString() -> NSMutableAttributedString {
-        self.textAttachment()
-            .asMutableAttributedString()
-            .font(self.font)
+        if useInYYText {
+            return self.asYYMutableAttributedString()
+                .font(self.font)
+        } else {
+            return self.textAttachment()
+                .asMutableAttributedString()
+                .font(self.font)
+        }
     }
 }
 
--- ZLBaseExtension/Classes/NSAttributedString+Delarative/NSTagWrapper.swift
+++ ZLBaseExtension/Classes/NSAttributedString+Delarative/NSTagWrapper.swift
@@ -12,6 +12,9 @@ import CoreGraphics
 public class NSTagWrapper: NSObject {
 
     var attributedString: NSAttributedString
+    
+    /// 标签的最大宽度
+    var preferMaxWidth: CGFloat = .greatestFiniteMagnitude
 
     // 圆角
     var cornerRadius: CGFloat = 0
@@ -24,9 +27,13 @@ public class NSTagWrapper: NSObject {
     //
     var backgroundColor: UIColor = UIColor.clear
     var backgroundImage: UIImage?
+    var backgroundImageFrame: CGRect = .zero
 
     // 内
     var edgeInsets: UIEdgeInsets = UIEdgeInsets.zero
+    
+    /// 是否用于YYText
+    var useInYYText: Bool = false
 
     public init(attributedString: NSAttributedString) {
         self.attributedString = attributedString
@@ -46,60 +53,58 @@ public class NSTagWrapper: NSObject {
 
     public func asImage() -> UIImage? {
 
-        let textSize = attributedString.boundingRect(with: CGSize(width: 10000, height: 10000),
+        let textSize = attributedString.boundingRect(with: CGSize(width: preferMaxWidth - edgeInsets.right - edgeInsets.left,
+                                                                  height: .greatestFiniteMagnitude),
                                                      options: .usesLineFragmentOrigin,
                                                      context: nil)
 
-        let textContainerSize = CGSize(width: round(textSize.width) + edgeInsets.right + edgeInsets.left + 1,
-                                       height: round(textSize.height) + edgeInsets.top + edgeInsets.bottom + 1)
+        let textContainerSize = CGSize(width: ceil(textSize.width + edgeInsets.right + edgeInsets.left),
+                                       height: ceil(textSize.height + edgeInsets.top + edgeInsets.bottom))
         let imageSize = CGSize(width: textContainerSize.width + 2 * borderWidth,
                                height: textContainerSize.height + 2 * borderWidth)
-        UIGraphicsBeginImageContextWithOptions(imageSize, false, UIScreen.main.scale)
-        let context = UIGraphicsGetCurrentContext()
-        //
-        let path = UIBezierPath(roundedRect: CGRect(origin: CGPoint(x: borderWidth, y: borderWidth),
-                                                    size: textContainerSize),
-                                byRoundingCorners: corner,
-                                cornerRadii: CGSize(width: cornerRadius, height: cornerRadius))
-        if borderWidth != 0 {
-            context?.setStrokeColor(borderColor.cgColor)
-            context?.setLineWidth(borderWidth)
-            path.stroke()
+    
+        let renderer = UIGraphicsImageRenderer(size: imageSize)
+        
+        let image = renderer.image { context in
+            
+            // 边框
+            let path = UIBezierPath(roundedRect: CGRect(origin: CGPoint(x: borderWidth, y: borderWidth),
+                                                        size: textContainerSize),
+                                    byRoundingCorners: corner,
+                                    cornerRadii: CGSize(width: cornerRadius, height: cornerRadius))
+            if borderWidth != 0 {
+                path.lineWidth = borderWidth
+                borderColor.setStroke()
+                path.stroke()
+            }
+
+            path.addClip()
+            
+            // 背景色
+            backgroundColor.setFill()
+            path.fill()
+            
+            // 背景图
+            if let backgroundImage = backgroundImage {
+                let frame = (self.backgroundImageFrame == .zero) ? CGRect(x: 0, y: 0, width: imageSize.width, height: imageSize.height) :  self.backgroundImageFrame
+                backgroundImage.draw(in: frame)
+            }
+            
+            // 文字
+            attributedString.draw(in: CGRect(x: edgeInsets.left + borderWidth,
+                                             y: edgeInsets.top + borderWidth,
+                                             width: ceil(textSize.width),
+                                             height: ceil(textSize.height)))
+            
         }
-
-        path.addClip()
-
-        // 背景色
-        context?.setFillColor(backgroundColor.cgColor)
-        path.fill()
-
-        // 背景图
-        if let backgroundImage = backgroundImage {
-            backgroundImage.draw(in: CGRect(x: 0, y: 0, width: imageSize.width, height: imageSize.height))
-        }
-
-        // 文字
-        attributedString.draw(in: CGRect(x: edgeInsets.left + borderWidth,
-                                         y: edgeInsets.top + borderWidth,
-                                         width: round(textSize.width) + 1,
-                                         height: round(textSize.height) + 1 ))
-
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-
-        UIGraphicsEndImageContext()
-
-        if let cgImage = image?.cgImage {
-            return UIImage(cgImage: cgImage,
-                           scale: UIScreen.main.scale,
-                           orientation: .up)
-        } else {
-            return nil
-        }
+        
+        return image
     }
 }
 
 public extension NSTagWrapper {
 
+    @discardableResult
     func text(_ text: String) -> NSTagWrapper {
         let attributedString = NSAttributedString(string: text,
                                                   attributes: [.font: UIFont.systemFont(ofSize: 14),
@@ -108,43 +113,69 @@ public extension NSTagWrapper {
         return self
     }
 
+    @discardableResult
     func attributedString(_ attributedString: NSAttributedString) -> NSTagWrapper {
         self.attributedString = attributedString
         return self
     }
 
+    @discardableResult
     func cornerRadius(_ cornerRadius: CGFloat) -> NSTagWrapper {
         self.cornerRadius = cornerRadius
         return self
     }
 
+    @discardableResult
     func corner(_ corner: UIRectCorner) -> NSTagWrapper {
         self.corner = corner
         return self
     }
 
+    @discardableResult
     func borderColor(_ color: UIColor) -> NSTagWrapper {
         self.borderColor = color
         return self
     }
 
+    @discardableResult
     func borderWidth(_ borderWidth: CGFloat) -> NSTagWrapper {
         self.borderWidth = borderWidth
         return self
     }
 
+    @discardableResult
     func backgroundColor(_ backgroundColor: UIColor) -> NSTagWrapper {
         self.backgroundColor = backgroundColor
         return self
     }
 
+    @discardableResult
     func backgroundImage(_ backgroundImage: UIImage) -> NSTagWrapper {
         self.backgroundImage = backgroundImage
         return self
     }
 
+    @discardableResult
     func edgeInsets(_ edgeInsets: UIEdgeInsets) -> NSTagWrapper {
         self.edgeInsets = edgeInsets
+        return self
+    }
+    
+    @discardableResult
+    func backgroundImageFrame(_ frame: CGRect) -> NSTagWrapper {
+        self.backgroundImageFrame = frame
+        return self
+    }
+    
+    @discardableResult
+    func preferMaxWidth(_ preferMaxWidth: CGFloat) -> NSTagWrapper {
+        self.preferMaxWidth = preferMaxWidth
+        return self
+    }
+    
+    @discardableResult
+    func useInYYText(_ useInYYText: Bool) -> NSTagWrapper {
+        self.useInYYText = useInYYText
         return self
     }
 }
